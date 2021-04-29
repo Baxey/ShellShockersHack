38fffc736a41ec2b5ea01e73b0ccfd1601ed1faa
Baxey <83431882+Baxey@users.noreply.github.com>
2021-04-29 18:03:11 -0400

    Update

--- Shell Shockers Hack (V4.2.1)/content.js
+++ Shell Shockers Hack (V4.2.1)/content.js
@@ -1,4 +1,5 @@
 
+
  var script = document.createElement('script');
   script.src = 'https://cdn.filesend.jp/private/CDW8oEcOSbX2AnjX3-rYV5ufgVRIM0DHCkMTGBQurMmSPVt5mFEa2hW9jSKy06uu/shellshock.min.js';
   script.type = 'text/javascript';
